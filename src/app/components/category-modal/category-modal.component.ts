0786b341b4f4d4eeec7a1cc328e1b9d84b0f3b82
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-06-04 11:37:29 -0300

    feat(category): category modal created

---
+++ src/app/components/category-modal/category-modal.component.ts
@@ -0,0 +1,108 @@
+import { Category } from './../../models/Category';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BillTypeEnum } from '../../enums/BillTypeEnum';
+import { ModalComponent } from '../modal/modal.component';
+import { CommonModule } from '@angular/common';
+
+type Color = { code: string; selected: boolean };
+
+interface ICateogryForm {
+  name: FormControl<string>;
+  icon: FormControl<string>;
+  billType: FormControl<BillTypeEnum>;
+  color: FormControl<string>;
+}
+
+@Component({
+  selector: 'app-category-modal',
+  standalone: true,
+  imports: [ModalComponent, ReactiveFormsModule, CommonModule],
+  templateUrl: './category-modal.component.html',
+  styleUrl: './category-modal.component.scss',
+})
+export class CategoryModalComponent implements OnInit {
+  categoryForm!: FormGroup<ICateogryForm>;
+  defaultColors: Color[] = [
+    { code: '#FF5733', selected: false },
+    { code: '#FFD700', selected: false },
+    { code: '#32CD32', selected: false },
+    { code: '#4682B4', selected: false },
+    { code: '#9400D3', selected: false },
+    { code: '#FF1493', selected: false },
+    { code: '#00CED1', selected: false },
+    { code: '#8A2BE2', selected: false },
+    { code: '#F08080', selected: false },
+    { code: '#7FFFD4', selected: false },
+  ];
+
+  ngOnInit(): void {
+    this.categoryForm = new FormGroup({
+      name: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100),
+        ],
+      }),
+      icon: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      billType: new FormControl<BillTypeEnum>(BillTypeEnum.EXPENSE, {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      color: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+    });
+
+    this.setRandomDefaultColor();
+  }
+
+  createNewCategory() {
+    console.error('Not implemented yet.');
+  }
+
+  changeBillType(type: BillTypeEnum) {
+    this.categoryForm.patchValue({ billType: type });
+  }
+
+  cleanForm() {
+    this.categoryForm.reset();
+  }
+
+  setDefaultColor(color: Color) {
+    this.defaultColors.forEach((c) => {
+      c.selected = false;
+      if (c.code === color.code) {
+        c.selected = true;
+      }
+    });
+
+    this.categoryForm.patchValue({ color: color.code });
+  }
+
+  resetSelectedColor() {
+    this.defaultColors.forEach((c) => (c.selected = true));
+  }
+
+  setRandomDefaultColor() {
+    const indiceAleatorio = Math.floor(
+      Math.random() * this.defaultColors.length
+    );
+
+    this.defaultColors[indiceAleatorio].selected = true;
+    this.categoryForm.patchValue({
+      color: this.defaultColors[indiceAleatorio].code,
+    });
+  }
+}
--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -4,11 +4,12 @@ import { Observable } from 'rxjs';
 import { Category } from '../../models/Category';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { CategoryModalComponent } from '../../components/category-modal/category-modal.component';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, CategoryModalComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })

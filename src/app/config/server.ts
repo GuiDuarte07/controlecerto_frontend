a14f5cdcfa609b062e23d6635f26b79c5d643dea
Guilherme Duarte <guilhduart.abr@gmail.como>
2024-09-01 19:47:45 -0300

    feat(mobile): login, register

--- src/app/config/server.ts
+++ src/app/config/server.ts
@@ -1,2 +1,2 @@
-/* export const serverConnectionString = 'http://localhost:8080/api'; */
-export const serverConnectionString = 'http://52.87.165.185:8080/api';
+export const serverConnectionString = 'http://localhost:8080/api';
+/* export const serverConnectionString = 'http://52.87.165.185:8080/api'; */

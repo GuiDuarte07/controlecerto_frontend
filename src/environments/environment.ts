7c864740b2829e8536c6f4f8e685cc5ae58440c2
Guilherme Duarte <guilhduart.abr@gmail.como>
2025-01-04 16:47:05 -0300

    fix: api route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverConnectionString: 'https://controlecerto.online/api',
+  serverConnectionString: 'https://api.controlecerto.online/api',
 };

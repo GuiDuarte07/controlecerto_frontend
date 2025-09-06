c9d7f9b0c2ab213596dcc67a434fb08ab71f6f3e
Guilherme Duarte <guilhduart.abr@gmail.como>
2025-09-06 15:45:42 -0300

    feat: implementa landing page completa com header, funcionalidades e footer

--- src/app/pages/default/default.component.ts
+++ src/app/pages/default/default.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [ButtonModule, AvatarModule],
+  imports: [ButtonModule, AvatarModule, RouterModule],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss',
 })
